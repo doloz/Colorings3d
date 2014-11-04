9942a327b3f68c720267768284ea25ba9394fbf7
Alex Doloz <doloz@inbox.ru>
2014-11-04 15:26:55 +0600

    Results save to file

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 var Scheme = require('./scheme.js');
+var fs = require('fs');
 
-var x = new Scheme(process.argv[2]);
+var colorsNumber = process.argv[2];
+if (colorsNumber === undefined) {
+	throw new Error('Нужно указать число цветов');
+}
+
+var filename = 'colorings' + colorsNumber + '.json';
+fs.writeFileSync(filename, '');
+
+var x = new Scheme(colorsNumber);
 x[0].left = x[1];
 x.autoFillAll();
+
+var startTime = new Date().getTime();
 x.enumerateAllSchemes(function(scheme) {
 	console.log("Найдено: ");
 	console.log(scheme.toString());
 	console.log('-----------------');
 });
+
+var jsonString = JSON.stringify(x.foundSchemes.map(function(scheme){
+	return scheme.toJSON()
+}));
+
+fs.appendFile(filename, jsonString, function (err) {
+	if (err) throw err;
+});
+
+console.log("Сохранено в файл", filename);
 console.log("Всего " + x.foundSchemes.length);
+
+var endTime = new Date().getTime();
+console.log("Время выполнения(мс): ",  endTime - startTime);
